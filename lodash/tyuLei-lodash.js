a6688088b38e3f6c32db6133608e1420f05c8bae
tyuLei <642568902@qq.com>
2020-04-29 22:57:38 +0800

    lodash

---
+++ lodash/tyuLei-lodash.js
@@ -0,0 +1,16 @@
+var tyuLei = {
+  isNull: function (val) {
+    if (val === null) {
+      return true
+    } else {
+      return false
+    }
+  },
+  isNaN: function (val) {
+    if (val !== val) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
